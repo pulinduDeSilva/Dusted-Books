0e9e73cfea0a429d715df21b73e299f041712c22
Sevin Kawsika <saween.kausika123@gmail.com>
2026-07-09 11:06:50 +0530

    finalise the frontend

--- client/dusted-books-app/src/App.tsx
+++ client/dusted-books-app/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { AuthProvider } from "./context/authContext";
+import { ThemeProvider } from "./context/themeContext";
+import { CartProvider } from "./context/cartContext";
 
 import ProtectedRoute from "./routes/protected";
 import GuestRoute from "./routes/GuestRoutes";
@@ -15,56 +17,90 @@ import UserManagement from "./components/admin/user/UserManagement";
 import BookManagement from "./components/admin/book/BookManagement";
 import UserCreateForm from "./components/admin/user/UserCreateForm";
 import BookUpload from "./components/admin/book/BookUpload";
+import Browse from "./pages/Browse";
+import Cart from "./pages/Cart";
+import BookDetails from "./pages/BookDetails";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route
-            path="/login"
-            element={
-              <GuestRoute>
-                <Login />
-              </GuestRoute>
-            }
-          />
+    <ThemeProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <CartProvider>
+            <Routes>
+              <Route
+                path="/login"
+                element={
+                  <GuestRoute>
+                    <Login />
+                  </GuestRoute>
+                }
+              />
 
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
 
-          <Route
-            path="/admin"
-            element={
-              <AdminRoute>
-                <AdminDashboard />
-              </AdminRoute>
-            }
-          >
-            <Route index element={<Navigate to="users/create" replace />} />
+              <Route
+                path="/browse"
+                element={
+                  <ProtectedRoute>
+                    <Browse />
+                  </ProtectedRoute>
+                }
+              />
 
-            {/* user-routes */}
-            <Route path="users" element={<UserManagement />}> 
-              <Route path="create" element={<UserCreateForm />} ></Route>
-            </Route>
-            {/* book-routes */}
-            <Route path="books" element={<BookManagement />} >
-              <Route path="add" element={<BookUpload />} ></Route>
-            </Route>
-          </Route>
+              <Route
+                path="/cart"
+                element={
+                  <ProtectedRoute>
+                    <Cart />
+                  </ProtectedRoute>
+                }
+              />
 
-          <Route path="/unauthorized" element={<Unauthorized />} />
+              <Route
+                path="/books/:id"
+                element={
+                  <ProtectedRoute>
+                    <BookDetails />
+                  </ProtectedRoute>
+                }
+              />
 
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+              <Route
+                path="/admin"
+                element={
+                  <AdminRoute>
+                    <AdminDashboard />
+                  </AdminRoute>
+                }
+              >
+                <Route index element={<Navigate to="users/create" replace />} />
+
+                {/* user-routes */}
+                <Route path="users" element={<UserManagement />}>
+                  <Route path="create" element={<UserCreateForm />} ></Route>
+                </Route>
+                {/* book-routes */}
+                <Route path="books" element={<BookManagement />} >
+                  <Route path="add" element={<BookUpload />} ></Route>
+                </Route>
+              </Route>
+
+              <Route path="/unauthorized" element={<Unauthorized />} />
+
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </CartProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
